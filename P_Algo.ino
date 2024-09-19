a7cebc1477f7bbe5993e0ff4daca6f64eabe0422
Gean <88801629+Geanrobert@users.noreply.github.com>
2024-09-18 21:40:11 -0300

    Add files via upload

---
+++ P_Algo.ino
@@ -0,0 +1,17 @@
+int Xm = 0
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+
+
+# Xm = (Ix2-Ix1)/(Ix1+Ix2)*(Lx/2)
